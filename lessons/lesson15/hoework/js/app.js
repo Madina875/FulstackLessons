f21f5897bd6c4d244d4a71e75f02d9ff98bc7fef
madina875 <evaevasmq@gmail.com>
2025-04-09 21:23:31 +0300

    hmw

--- lessons/lesson15/hoework/js/app.js
+++ lessons/lesson15/hoework/js/app.js
@@ -1,43 +1,87 @@
-function tozala(arr) {}
-
-let Input = ["olma", "dastur", "kut", "sudrat", "tuk", "mola"];
-let mymap = new Map();
-// Output: ["olma", "dastur", "kut"];
-for (const element of Input) {
-  mymap.set(element, element.split(""));
-  console.log(element);
-}
-console.log(mymap);
-for (let key in Input) {
-  console.log(Input.at(key));
-  for (let keyin of Input.at(key)) {
-    if (Input.at(key).includes(keyin)) {
-      console.log(true);
-      key += 1;
-    } else {
-      console.log(false);
+//1
+function tozala(arr) {
+  const map = new Map();
+  arr.forEach((soz) => {
+    const kalit = [...soz].sort().join("");
+    if (!map.has(kalit)) {
+      map.set(kalit, soz);
     }
+  });
+  return [...map.values()];
+}
+const massiv = ["olma", "dastur", "kut", "sudrat", "tuk", "mola"];
+const tozalangan = tozala(massiv);
+console.log(tozalangan);
+
+//2
+
+class Avto {
+  constructor(model, marka, year) {
+    this.model = model;
+    this.marka = marka;
+    this.year = year;
   }
 }
-//// 3
-// function odamSoni(odamlarholati) {
-//   let passangers = 0;
+class YengilAvto extends Avto {
+  constructor(model, marka, year) {
+    super(model, marka, year);
+    this.model = model;
+    this.marka = marka;
+    this.year = year;
+  }
+}
+class YukAvto extends Avto {
+  constructor(model, marka, year) {
+    super(model, marka, year);
+    this.model = model;
+    this.marka = marka;
+    this.year = year;
+  }
+}
+class garaj {
+  constructor(joylarSoni) {
+    this.joylarSoni = joylarSoni;
+    this.avtolar = new Array();
+  }
 
-//   for (const [entr, out] of odamlarholati) {
-//     passangers += entr;
-//     passangers -= out;
-//   }
+  joyla(yangiAvto) {
+    if (this.avtolar.length != this.joylarSoni) {
+      this.avtolar.push(yangiAvto.constructor.name);
+      console.log("saqlandi");
+    } else {
+      console.log("oshib ketti");
+    }
+  }
+  chiqar() {
+    console.log(this.avtolar);
+  }
+}
+const b = new garaj(2);
+b.joyla(new YengilAvto("asdfgh", "asdfgh", 2000));
+b.joyla(new YengilAvto("asdfgh", "asdfgh", 2000));
+b.joyla(new YengilAvto("asdfgh", "asdfgh", 2000));
+b.chiqar();
 
-//   return passangers;
-// }
+// 3
 
-// let odamsoni = [
-//   [3, 0],
-//   [9, 1],
-//   [4, 8],
-//   [12, 2],
-//   [6, 1],
-//   [7, 8],
-// ];
+function odamSoni(odamlarholati) {
+  let passangers = 0;
 
-// console.log(odamSoni(odamsoni));
+  for (const [entr, out] of odamlarholati) {
+    passangers += entr;
+    passangers -= out;
+  }
+
+  return passangers;
+}
+
+let odamsoni = [
+  [3, 0],
+  [9, 1],
+  [4, 8],
+  [12, 2],
+  [6, 1],
+  [7, 8],
+];
+
+console.log(odamSoni(odamsoni));
